1856a2fae3c55ae99cfebf67e3b5066c59140166
BT_1N3_04 <fasfaFDASDF>
2019-02-07 11:13:23 -0500

    gdafgsaf

--- src/com/company/calc.java
+++ src/com/company/calc.java
@@ -21,9 +21,15 @@ public class calc extends Application  {
 
     long start = System.nanoTime();
     int numclicked = 0;
+    String problem = "";
+
+    public String getProblem() {
+        return problem;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         primaryStage.setTitle("HBox Experiment 1");
         GridPane canvas = new GridPane();
 
@@ -36,7 +42,60 @@ public class calc extends Application  {
         TextField num2 = new TextField();
         Button lastInp = new Button("Last");
         Button equals = new Button("=");
-        Label answer = new Label("answer");
+        //Label answer = new Label("answer");
+
+        plus.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                problem = ""+num1.getText()+" + "+num2.getText();
+                System.out.println(getProblem());
+            }
+        });
+        minus.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                problem = ""+num1.getText()+" - "+num2.getText();
+                System.out.println(getProblem());
+            }
+        });
+        multiply.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                problem = ""+num1.getText()+" * "+num2.getText();
+                System.out.println(getProblem());
+            }
+        });
+        divide.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                problem = ""+num1.getText()+" / "+num2.getText();
+                System.out.println(getProblem());
+            }
+        });
+        clear.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               num1.setText("");
+               num2.setText("");
+            }
+        });
+        equals.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               if(getProblem().substring(2,3).equals("+")){
+                   System.out.println(Integer.parseInt(getProblem().substring(0,1)) + Integer.parseInt(getProblem().substring(4)));
+               }
+                if(getProblem().substring(2,3).equals("-")){
+                    System.out.println(Integer.parseInt(getProblem().substring(0,1)) - Integer.parseInt(getProblem().substring(4)));
+                }
+                if(getProblem().substring(2,3).equals("*")){
+                    System.out.println(Integer.parseInt(getProblem().substring(0,1)) * Integer.parseInt(getProblem().substring(4)));
+                }
+                if(getProblem().substring(2,3).equals("/")){
+                    System.out.println(Integer.parseInt(getProblem().substring(0,1)) / Integer.parseInt(getProblem().substring(4)));
+                }
+            }
+        });
 
         canvas.add(divide, 0, 0);
         canvas.add(multiply, 1,0);
@@ -48,7 +107,7 @@ public class calc extends Application  {
         canvas.add(num2,1,2);
         canvas.add(lastInp,0,3,2,1);
         canvas.add(equals,0,3,2,1);
-        canvas.add(answer,0,3,2,1);
+        //canvas.add(answer,0,3,2,1);
 
         num1.setPrefWidth(70);
         num2.setPrefWidth(70);
@@ -59,7 +118,7 @@ public class calc extends Application  {
         clear.setPrefWidth(140);
         equals.setPrefWidth(140);
         lastInp.setPrefWidth(140);
-        answer.setPrefWidth(140);
+        //answer.setPrefWidth(140);
 
 
         Scene scene = new Scene(canvas, 500, 500);
